f71eec4972feace7265654c3c5c0dd3019310ad3
Lucas Pontes <lucaspontessantana@gmail.com>
2023-08-14 10:15:21 -0300

    adicionado teste de error na miração

--- Sources/App/Migrations/CreateTodo.swift
+++ Sources/App/Migrations/CreateTodo.swift
@@ -2,13 +2,26 @@ import Fluent
 
 struct CreateTodo: AsyncMigration {
     func prepare(on database: Database) async throws {
-        try await database.schema("todos")
-            .id()
-            .field("title", .string, .required)
-            .create()
+        do {
+            try await database.schema("todos")
+                .id()
+                .field("title", .string, .required)
+                .create()
+        } catch {
+            throw MigrationError.creationFailed(reason: error.localizedDescription)
+        }
     }
 
     func revert(on database: Database) async throws {
-        try await database.schema("todos").delete()
+        do {
+            try await database.schema("todos").delete()
+        } catch {
+            throw MigrationError.revertFailed(reason: error.localizedDescription)
+        }
     }
+}
+
+enum MigrationError: Error {
+    case creationFailed(reason: String)
+    case revertFailed(reason: String)
 }
