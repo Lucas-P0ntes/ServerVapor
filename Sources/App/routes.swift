7fe300a6e3cb503245cd3216ca82f217062f8977
Lucas Pontes <lucaspontessantana@gmail.com>
2023-08-17 13:52:09 -0300

    Tentando fazer post de img 2

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,21 +2,30 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-  
+    
     /// config max upload file size
-       app.routes.defaultMaxBodySize = "100mb"
-       
-       /// setup public file middleware (for hosting our uploaded files)
-       app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-       
-    app.post("upload") { req -> EventLoopFuture<String> in
+    app.routes.defaultMaxBodySize = "100mb"
+    
+    /// setup public file middleware (for hosting our uploaded files)
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.post("upload") { req -> EventLoopFuture<View> in
         struct Input: Content {
             var file: File
         }
         let input = try req.content.decode(Input.self)
-        
-        let path = app.directory.publicDirectory + input.file.filename
-        
+
+        guard input.file.data.readableBytes > 0 else {
+            throw Abort(.badRequest)
+        }
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-m-d-HH-MM-SS-"
+        let prefix = formatter.string(from: .init())
+        let fileName = prefix + input.file.filename
+        let path = app.directory.publicDirectory + fileName
+        let isImage = ["png", "jpeg", "jpg", "gif"].contains(input.file.extension?.lowercased())
+
         return req.application.fileio.openFile(path: path,
                                                mode: .write,
                                                flags: .allowFileCreation(posixMode: 0x744),
@@ -27,12 +36,17 @@ func routes(_ app: Application) throws {
                                              eventLoop: req.eventLoop)
                     .flatMapThrowing { _ in
                         try handle.close()
-                        return input.file.filename
+                    }
+                    .flatMap {
+                        req.leaf.render(template: "result", context: [
+                            "fileUrl": .string(fileName),
+                            "isImage": .bool(isImage),
+                        ])
                     }
             }
     }
     
-
+    
     app.get{ req -> EventLoopFuture<View> in
         return req.view.render("hello", ["name": "Leaf"])
     }
