57415cd27d272ddb61b63b5689c79479284858cd
Lucas Pontes <lucaspontessantana@gmail.com>
2023-08-20 16:04:30 -0300

    test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,7 +9,6 @@ func routes(_ app: Application) throws {
     /// setup public file middleware (for hosting our uploaded files)
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-
     app.get{ req -> EventLoopFuture<View> in
         return req.view.render("hello", ["name": "Leaf"])
     }
