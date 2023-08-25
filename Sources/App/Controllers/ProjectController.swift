559c2c68a3a939e9de205620fefb007ed01ea144
Lucas Pontes <lucaspontessantana@gmail.com>
2023-08-25 15:48:01 -0300

    add img

--- Sources/App/Controllers/ProjectController.swift
+++ Sources/App/Controllers/ProjectController.swift
@@ -75,7 +75,36 @@ struct ProjectController: RouteCollection {
     
     func update(req: Request)async throws -> HTTPStatus {
         
-        return .accepted
+        let data = try await req.content.decode(ImageUploadData.self)
+           let projectId = req.parameters.get("id", as: UUID.self) // Obtém o ID do projeto da URL
+           
+           guard let existingProject = try await ProjectModel.find(projectId, on: req.db) else {
+               throw Abort(.notFound) // Se o projeto não existe, retorne um erro 404
+           }
+
+           let imageFolder = "/Public/images/"
+           // Resto do código de manipulação de imagem...
+        
+        let id = UUID()
+        let imageNameIcon = "\(id)Icon.jpg"
+        let imageNamePicture = "\(id)Picture.jpg"
+        // It can be a path outside the main program
+        
+        let pathIcon = req.application.directory.workingDirectory + imageFolder + imageNameIcon
+        let pathPicture = req.application.directory.workingDirectory + imageFolder + imageNamePicture
+
+        
+        try await req.fileio.writeFile(.init(data: data.imgScreenshot), at: pathPicture)
+        try await req.fileio.writeFile(.init(data: data.imgIcon), at: pathIcon )
+        
+
+           // Atualizar os campos relevantes do projeto existente
+           existingProject.imgIcon = "https://api-project-academy-9cf71ea0cac6.herokuapp.com/images/\(imageNameIcon)"
+           existingProject.imgScreenshot = "https://api-project-academy-9cf71ea0cac6.herokuapp.com/images/\(imageNamePicture)"
+
+           try await existingProject.update(on: req.db) // Atualizar o projeto no banco de dados
+           
+           return .ok // Ou .accepted, ou qualquer outro status apropriado
     }
     
     func delete(req: Request) async throws -> HTTPStatus {
